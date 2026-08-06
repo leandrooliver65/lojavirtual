0512a90d23f35eb14e40b3b9824f2cb449d8e427
Leandro de Oliveira Pinto <leandro.oliver65@outlook.com>
2026-08-05 21:30:42 -0300

    fix github pages deploy

--- build.js
+++ build.js
@@ -1,14 +1,17 @@
 const fs = require('fs');
 const path = require('path');
 
-const sourceDir = path.join(__dirname, 'public');
+const sourcePublicDir = path.join(__dirname, 'public');
 const outputDir = path.join(__dirname, 'build');
 
-if (!fs.existsSync(outputDir)) {
-  fs.mkdirSync(outputDir, { recursive: true });
+if (fs.existsSync(outputDir)) {
+  fs.rmSync(outputDir, { recursive: true, force: true });
 }
+fs.mkdirSync(outputDir, { recursive: true });
 
 function copyRecursive(src, dest) {
+  fs.mkdirSync(dest, { recursive: true });
+
   const entries = fs.readdirSync(src, { withFileTypes: true });
 
   for (const entry of entries) {
@@ -16,9 +19,6 @@ function copyRecursive(src, dest) {
     const destPath = path.join(dest, entry.name);
 
     if (entry.isDirectory()) {
-      if (!fs.existsSync(destPath)) {
-        fs.mkdirSync(destPath, { recursive: true });
-      }
       copyRecursive(srcPath, destPath);
     } else if (entry.isFile()) {
       fs.copyFileSync(srcPath, destPath);
@@ -26,12 +26,24 @@ function copyRecursive(src, dest) {
   }
 }
 
-copyRecursive(sourceDir, outputDir);
+copyRecursive(sourcePublicDir, path.join(outputDir, 'public'));
+
+const sourceIndex = path.join(__dirname, 'index.html');
+const targetIndex = path.join(outputDir, 'index.html');
+if (fs.existsSync(sourceIndex)) {
+  fs.copyFileSync(sourceIndex, targetIndex);
+}
 
 const source404 = path.join(__dirname, 'public', '404.html');
 const target404 = path.join(outputDir, '404.html');
 if (fs.existsSync(source404)) {
   fs.copyFileSync(source404, target404);
+}
+
+const sourceNoJekyll = path.join(__dirname, '.nojekyll');
+const targetNoJekyll = path.join(outputDir, '.nojekyll');
+if (fs.existsSync(sourceNoJekyll)) {
+  fs.copyFileSync(sourceNoJekyll, targetNoJekyll);
 }
 
 console.log('Build concluído em ./build');
