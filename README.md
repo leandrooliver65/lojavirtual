4aefb87f385768bf817e7387308ac12e62717f8e
Leandro de Oliveira Pinto <leandro.oliver65@outlook.com>
2026-08-05 21:26:14 -0300

    fix css

---
+++ README.md
@@ -0,0 +1,15 @@
+# Vela & Luz
+
+Projeto web Node.js para a loja Vela & Luz.
+
+## Como executar
+
+- Instale as dependências com `npm install`
+- Inicie o servidor com `npm start`
+- Acesse http://localhost:3000/
+
+## Estrutura
+
+- `server.js`: ponto de entrada do servidor Express
+- `public/`: arquivos estáticos servidos pela aplicação
+- `build.js`: copia os arquivos da pasta pública para a pasta `build`
--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 const express = require('express');
+const fs = require('fs');
 const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname)));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  const indexPath = path.join(__dirname, 'index.html');
+  const rootIndexPath = path.join(__dirname, 'index.html');
   const publicIndexPath = path.join(__dirname, 'public', 'index.html');
 
-  if (require('fs').existsSync(indexPath)) {
-    res.sendFile(indexPath);
-  } else if (require('fs').existsSync(publicIndexPath)) {
+  if (fs.existsSync(rootIndexPath)) {
+    res.sendFile(rootIndexPath);
+  } else if (fs.existsSync(publicIndexPath)) {
     res.sendFile(publicIndexPath);
   } else {
     res.status(404).send('Página não encontrada');
